e2f977892cf423eec74cd205782a5e925f04ae5f
zendo <zendo@example.com>
2020-01-20 16:38:17 +0800

    OPT gzip script

--- scripts/gzip.js
+++ scripts/gzip.js
@@ -12,16 +12,32 @@ function gzipFile(fileName) {
   })
 }
 
-try {
-  const items = fs.readdirSync('./dist')
-  console.log('\nbuild gzip files:')
-  items.forEach(fileName => {
-    if (/\.js$/.test(fileName)) {
+function getWillGzipFiles() {
+  try {
+    const items = fs.readdirSync('./dist')
+    // Filter files which will do gzip.
+    return items.filter(fileName => {
+      if (/\.js$/.test(fileName)) {
+        return true
+      }
+    })
+  } catch (e) {
+    throw e
+  }
+}
+
+function main() {
+  try {
+    const files = getWillGzipFiles()
+    console.log('\nbuild gzip files:')
+    files.forEach(fileName => {
       console.log(`- generating "${fileName}.gz" file...`)
       gzipFile(`./dist/${fileName}`)
-    }
-  })
-  console.log('\n')
-} catch (e) {
-  console.log('Error: ', e)
+    })
+    console.log('\n')
+  } catch (e) {
+    console.log('Error: ', e)
+  }
 }
+
+main()
